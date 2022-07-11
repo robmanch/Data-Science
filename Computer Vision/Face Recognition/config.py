9195d3d0c1db4c9b4fd7c1a92cb693fdf7a4d57c
Robin Manchanda <robinm.ecil@gmail.com>
2022-07-11 11:42:49 -0400

    Add files via upload

---
+++ Computer Vision/Face Recognition/config.py
@@ -0,0 +1,7 @@
+import os
+
+CWD = os.getcwd()
+DATA_PATH = os.path.join(CWD, 'data')
+ANC_PATH = os.path.join(DATA_PATH, 'anchor')
+POS_PATH = os.path.join(DATA_PATH, 'positive')
+NEG_PATH = os.path.join(DATA_PATH, 'negative')
